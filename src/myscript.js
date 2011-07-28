7986fb2848ca0d3578ba4e9db0b70ef35a1c2c0c
Russell Leggett <russell.leggett@gmail.com>
2011-07-28 16:27:29 -0400

    updated to new version of CardUpload

--- src/myscript.js
+++ src/myscript.js
@@ -14,9 +14,9 @@
         // Firefox supports PNG and JPEG. You could check img.src to
         // guess the original format, but be aware the using "image/jpg"
         // will re-encode the image.
-        var dataURL = canvas.toDataURL("image/png");
+        var dataURL = canvas.toDataURL("image/jpeg");
     
-        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+        return dataURL.replace(/^data:image\/(png|jpeg);base64,/, "");
     }
 
     function addToDeck(){
@@ -24,10 +24,10 @@
         function textForLabel(label){
             return cardInfo.has(".label:contains('" + label + "')").find(".value").text().trim();
         }
+        //get all the basic text stuff
         var cardUploadJson = {
             name:textForLabel("Card Name"),
             types:textForLabel("Types"),
-            text:textForLabel("Card Text"),
             flavorText:textForLabel("Flavor Text"),
             powerToughness:textForLabel("P/T"),
             rarity:textForLabel("Rarity"),
@@ -35,21 +35,37 @@
             cardNumber:textForLabel("Card #"),
             artist:textForLabel("Artist")
         };
+        
+        //special setup for text in case of multiple lines
+        var textRow = cardInfo.has(".label:contains('Card Text')").find(".value .cardtextbox");
+        var cardText = textRow.map(function(){
+            return $(this).text().trim();
+        }).get().join("\n");
+        cardUploadJson.text = cardText;
+        
+        //mana cost and color
         var manaCost = cardInfo.has(".label:contains('Mana Cost')").find(".value");
         if(manaCost.size()){
             var colors = ["White","Blue","Black","Red","Green"];
             var hasColors = [];
             colors.forEach(function(color){
-                if(manaCost.has("img[alt="+color+"]").size()){
+                if(manaCost.has("img[alt~="+color+"]").size()){
                     hasColors.push(color);
                 }
             });
-            var cardColor = hasColors.length==0?"colorless"
-                          : hasColors.length>1? "gold"
-                          : hasColors[0];
+            var cardColor = hasColors.length==0?"COLORLESS"
+                          : hasColors.length>1? "GOLD"
+                          : hasColors[0].toUpperCase();
             cardUploadJson.color = cardColor;
-            cardUploadJson.cost = textForLabel("Converted Mana Cost")
+            cardUploadJson.cost = parseInt(textForLabel("Converted Mana Cost"));
         }
+        
+        //analyze gathered data to set some flags
+        var primaryType = cardUploadJson.types.split("—")[0];//em dash
+        cardUploadJson.land = primaryType.indexOf("Land") >= 0;
+        cardUploadJson.creature = primaryType.indexOf("Creature") >= 0;
+        cardUploadJson.permanent = primaryType.search(/(Land|Creature|Artifact|Enchantment)/) >= 0;
+        
         //now that the textual data is sucked in, lets get that image
         var img = $(".cardDetails .leftCol img").get(0);
         cardUploadJson.imageData = getBase64Image(img);
